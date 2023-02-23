159ff5a7ae0ff81c9e7dbd64632f3f1ca8780e2a
Prasath <100816296+prasath-23@users.noreply.github.com>
2023-02-23 15:44:15 +0530

    Add files via upload

---
+++ Reference/jpa/src/main/java/com/example/jpa/Repository/StudentRepo.java
@@ -0,0 +1,8 @@
+package com.example.jpa.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.jpa.Data.Student;
+
+public interface StudentRepo extends JpaRepository< Student , Integer > {
+
+} 
