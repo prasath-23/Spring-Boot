841855f1742285a82b4a1da8a6bf421759743049
Prasath <100816296+prasath-23@users.noreply.github.com>
2023-02-23 17:11:06 +0530

    Delete jpa directory

--- jpa/src/main/java/com/example/jpa/Repository/StudentRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.jpa.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.jpa.Data.Student;
-
-public interface StudentRepo extends JpaRepository< Student , Integer > {
-
-} 
